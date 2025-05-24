b133c53e1ceeb0f56cd1f38d2a3be6240c888def
Teste <silly>
2025-05-24 13:39:32 -0300

    oops i messed up

--- silly/script.js
+++ silly/script.js
@@ -3,14 +3,14 @@ const tickspersecond = 30
 const gel = (name) => document.getElementById(name)
 let player = {
     sillytime: 0,
-    lasttick: Date.time()
+    lasttick: Date.now()
 }
 function showtext(){
   gel("sillytime").textContent = player.sillytime.toFixed(2)+"s"
 }
 setInterval(() => {
-  ticktime = Date.time() - lasttick
-  player.lasttick = Date.time()
+  ticktime = Date.now() - lasttick
+  player.lasttick = Date.now()
   while(ticktime > 1/tickspersecond){
     player.sillytime += 1/tickspersecond
 
