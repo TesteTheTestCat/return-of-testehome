a97baa4f46ab2b202b8bec328ed250f632520e80
Teste <silly>
2025-05-24 13:52:27 -0300

    more stuff idk man

---
+++ silly/format.js
@@ -0,0 +1,40 @@
+export function format(x,l=true){
+  let eee = ["*10^","10^"]
+  if (l){
+    let eee = ["e","e"]
+  }
+  // very low value
+  if (x < 1e-6){
+    return "0"
+  }
+  // 1e-6 to 1
+  else if (x < 1){
+    return "1/"+(x**-1).toFixed(2)
+  }
+  // 0.1 to 1000
+  else if(x < 1000){
+    return x.toFixed(2)
+  }
+  // 1e3 to 1e9
+  else if (x < 1e9){
+    return x.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, "'")
+  }
+  // post-1e128
+  else if (x < 1e128){
+    let log10x = Math.floor(Math.log10(x))
+    let xoverl10x = x/(10**log10x)
+    if (xoverl10x >= 10){
+      return ((xoverl10x/10).toFixed(2)+eee[0]+((log10x+1)))
+    }
+    else {
+      return ((xoverl10x).toFixed(2)+eee[0]+((log10x)))
+    }
+  }
+  else if (x >= 1e128){
+    let log10x = Math.floor(Math.log10(x))
+    return eee[1]+(log10x.toFixed(5))
+  }
+}
+export function intformat(x){
+  return x.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, "'")
+}
--- silly/script.js
+++ silly/script.js
@@ -1,3 +1,4 @@
+import { format, intformat } from "./format.js";
 const framespersecond = 60
 const tickspersecond = 30
 const gel = (name) => document.getElementById(name)
@@ -6,10 +7,11 @@ let player = {
     lasttick: Date.now()
 }
 function showtext(){
-  gel("sillytime").textContent = player.sillytime.toFixed(2)+"s"
+  gel("sillytime").textContent = format(player.sillytime)+"s"
 }
+ticktime = 0
 setInterval(() => {
-  ticktime = Date.now() - lasttick
+  ticktime += (Date.now() - player.lasttick)/1000
   player.lasttick = Date.now()
   while(ticktime > 1/tickspersecond){
     player.sillytime += 1/tickspersecond
