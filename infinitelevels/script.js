c46a3f04fadf8b23636ec44edea2e6edd1cdb36e
Teste <silly>
2025-09-06 20:09:58 -0300

    mreow mrrp wawa purr mroew

--- infinitelevels/script.js
+++ infinitelevels/script.js
@@ -6,8 +6,8 @@ let player = {
     xp: [0],
 }
 function levelRequire(lv,t){
-  lvp = lv
-  p = 100-(10*t)
+  let lvp = lv
+  let p = 100-(10*t)
   if (p < 80){p = 80}
   if (lv > 1000){lvp *= 1+(0.01*(lv-1000))}
   return (p*(1+(0.05*lvp)))
@@ -22,6 +22,6 @@ setInterval(() => {
     player.levels[0] += 1
   }
   gel("leveltime").innerHTML = `Level ${intformat(player.levels[0])}, ${format(player.xp[0])}/${format(levelRequire(player.levels[0],0))}
-  <progress max="${levelRequire(player.levels[0],0)}" value="${player.xp[0]}"><br>`
+  <br>0% boost from higher tiers (${1} XP/s)<br><progress max="${levelRequire(player.levels[0],0)}" value="${player.xp[0]}"><br>`
   ticktime = 0
 },1000/60)
