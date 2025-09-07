81a8f97e9c44ed2a8fbaebf2eb4c19f63f43694d
Teste <silly>
2025-09-07 11:33:51 -0300

    just APPEND it already

--- infinitelevels/script.js
+++ infinitelevels/script.js
@@ -14,11 +14,11 @@ function levelRequire(lv,t){
 }
 function makeInnerHTMLForLevelTime(){
   let j = ""
-  let k = ""
   for (let i = 0; i < player.levels.length; i++){
   j += `Level ${intformat(player.levels[i])}, ${format(player.xp[i])}/${format(levelRequire(player.levels[i],i))}
   <br>0% boost from higher tiers<br><progress max="${levelRequire(player.levels[i],i)}" value="${player.xp[i]}"><br>`
   }
+  console.log(j)
   return j
 }
 const gel = (name) => document.getElementById(name)
